f66d459edd17eb23ebaae56739abb56d9a0ae42e
kerbinagent <wanghouze@gmail.com>
2016-01-28 18:10:39 -0600

    type definitions for history of province and countries
    only defined map-relevant information

---
+++ HistoryType.hs
@@ -0,0 +1,26 @@
+module HistoryType where
+import Data.Word
+type HistoryDate = (Word16,Word8,Word8)
+data Monarch = Monarch {
+monarchname::String,
+birthdate::HistoryDate,
+deathdate::HistoryDate,
+getAdm::Word8,
+getDip::Word8,
+getMil::Word8
+}
+
+data Country = Country {
+countryname::String,
+religion::Int,
+capital::Word16,
+leaders::[Monarch]
+}
+
+data ProvinceHistory = PHistory {
+provinceID::Int,
+baseDevelopment::Word8,
+provinceReligion::Int,
+hre::Bool,
+controllers::[(String,HistoryDate)]
+}
