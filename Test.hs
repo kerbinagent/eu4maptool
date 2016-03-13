6e3b45c272fac124f349995a4d904b850bae3ae9
kerbinagent <wanghouze@gmail.com>
2016-03-12 21:19:38 -0600

    testing purpose

--- Test.hs
+++ Test.hs
@@ -1,34 +1,41 @@
-import BuildData
-import Parser
-import Codec.Picture
-import Data.Array
-import ImageTracer
 import Beijing
-import MapType
-import qualified Data.Map.Strict as Map
-import Data.Maybe
+import Drawing
+import ImageTracer
+import Alphabet as ALP
+import Triangulation as TRI
+import qualified Graphics.Gloss.Interface.IO.Game as GS
+import Data.Monoid ((<>))
 
-getsmap :: IO ShapeMap
-getsmap = do
-  g <- readFile "resources/definition.csv"
-  let defMap = parseRDef g
-  Right (ImageRGB8 bmp) <- readBitmap "resources/provinces.bmp"
-  s <- readFile "resources/sealist"
-  let seamap = Map.fromList $ zip (map read (lines s)) (repeat True)
-  let smap = buildShape defMap bmp seamap
-  return smap
+renderBJ :: (Float,Float,Float) -> IO GS.Picture
+renderBJ (x,y, zoom) = return $ GS.scale zoom zoom result where
+  ctp =  getBezierControl $ optimalPolygon beijing
+  (a,b,c) = getOutline curve
+  centerr = getCenterRad a b c
+  tctp = map (transPath x y) ctp
+  curve = (concatMap (init . drawBezier (4/zoom))) tctp
+  colorpart = (mconcat . map GS.polygon . TRI.triangulate) curve
+  typepart = renderName (0.05) curve "Beijing"
+  roundd = uncurry GS.translate (fst centerr) (GS.scale 0.1 0.1 $ GS.text ".")
+  result = roundd <> colorpart <> (GS.color GS.red typepart) <> (GS.color GS.green ((uncurry GS.translate a (GS.scale 0.05 0.05 $ GS.text "1")) <> uncurry GS.translate b (GS.scale 0.05 0.05 $ GS.text "2") <> uncurry GS.translate c (GS.scale 0.05 0.05 $ GS.text "3")))
+
+stepW _ = return
+
+handle (GS.EventKey (GS.SpecialKey GS.KeyUp) GS.Up _ _) (x,y,z) = return (x,y-10,z)
+handle (GS.EventKey (GS.SpecialKey GS.KeyDown) GS.Up _ _) (x,y,z) = return (x,y+10,z)
+handle (GS.EventKey (GS.SpecialKey GS.KeyLeft) GS.Up _ _) (x,y,z) = return (x-10,y,z)
+handle (GS.EventKey (GS.SpecialKey GS.KeyRight) GS.Up _ _) (x,y,z) = return (x+10,y,z)
+handle (GS.EventKey (GS.SpecialKey GS.KeyPageUp) GS.Up _ _) (x,y,z) = return (x,y-10,z*1.2)
+handle (GS.EventKey (GS.SpecialKey GS.KeyPageDown) GS.Up _ _) (x,y,z) = return (x,y-10,z*0.8)
+handle _ b = return b
+
 
 main :: IO ()
 main = do
-  c <- readFile "resources/vanillalu"
-  b <- readFile "resources/vanillard"
-  smap <- getsmap
-  let (_,(i,j)) = bounds smap
-  let lumap = Map.fromAscList $ map read (lines c)
-  let drmap = Map.fromAscList $ map read (lines b)
-  let pmap = buildLongPath smap lumap drmap
-  --mapM_ print $ Map.toList drmap
-  mapM_ print $ zip [1..3003] $ map (length . provBezier pmap) [1..3003]
-  --putStrLn "%!PS\n1 setlinecap\n"
-  --mapM_ putStr result
-  --putStrLn "showpage"
+  GS.playIO
+    (GS.InWindow "EUIV Map Editor" (1280,720) (0,0))
+    GS.azure
+    1
+    (4640,723,4)
+    renderBJ
+    handle
+    stepW
